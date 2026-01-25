749615915fa88730d7695d34813af0c20e023f2d
Jean-Mark Wright <jeanmark.wright@gmail.com>
2026-01-25 14:07:23 -0500

    Building static files to be served by Django
    Added a command that builds the static files
    and copies them over to Django's static assets
    and also copies over the template for index.html

--- build.sh
+++ build.sh
@@ -4,13 +4,18 @@ echo "Building frontend for production..."
 
 # Build the React frontend
 cd frontend
-npm install
-npm run build
+yarn install
+yarn run build
 
 # Copy build files to Django static directory
 cd ..
 mkdir -p backend/staticfiles
 cp -r frontend/build/static/* backend/staticfiles/
 cp frontend/build/index.html backend/templates/
+
+# Also copy to root for Heroku
+mkdir -p staticfiles templates
+cp -r frontend/build/static/* staticfiles/
+cp frontend/build/index.html templates/
 
 echo "Frontend build complete!"
